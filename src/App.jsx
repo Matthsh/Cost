abbb890b2501c6a97b5f76940c024cad22691403
Matthsh <mattheusmedina@outlook.com>
2022-09-30 14:43:01 -0300

    navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import Rotear from "./router/Rotear.jsx"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Contato from './components/pages/Contato.jsx'
+import Home from './components/pages/Home.jsx'
+import Company from './components/pages/Company.jsx'
+import Navbar from './components/layout/Navbar.jsx'
+import Footer from './components/layout/Footer.jsx'
+import Container from './components/layout/Container.jsx'
+import Projects from './components/pages/Projects.jsx'
+
 
 function App() {
   return (
     <div className="App">
-      <Rotear />
+      <Router>
+        <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/company" element={<Company />}></Route>
+            <Route path="/contato" element={<Contato />}></Route>
+            <Route path="/projects" element={<Projects />}></Route>
+          </Routes>
+        </Container>
+        <Footer />
+      </Router>
     </div>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,3 @@
-import Header from "../layout/Header";
-
 export default function Home() {
     return (
         <>
