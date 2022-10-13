41169fed2a13284f463de19038f66818a2055c29
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-13 16:02:21 -0300

    ✨ Mecanismo para adicionar novos projetos ao BD

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -1,11 +1,21 @@
 import styles from './select.module.css'
 
-export default function Select({text, name, option, handleOnChange, value }) {
+export default function Select({text, name, options, handleOnChange, value }) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
+            <select 
+                name={name} 
+                id={name} 
+                onChange={handleOnChange} 
+                value={value || ''}
+            >
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
