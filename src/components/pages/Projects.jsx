96410292ed9a7b6d4bc39feb768094d4ae0c91ed
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-17 00:11:15 -0300

    🚧 dashboard projetos

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,16 +1,25 @@
-import {useLocation} from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import Message from "../layout/Message";
+import styles from './Projects.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton';
 
 export default function Projects() {
     const location = useLocation()
     let message = ''
-    if(location.state) {
+    if (location.state) {
         message = location.state.message
     }
     return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message msg={message} type="success"/>}
+        <div className={styles.project_container}>
+            <div className={styles.tittle_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton text="Criar Projeto" to="/newproject"></LinkButton>
+            </div>
+            {message && <Message msg={message} type="success" />}
+            <Container customClass="start">
+                <p>Projetos...</p>
+            </Container>
         </div>
     )
 }
