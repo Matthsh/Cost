062d3666e5a429eccab0e5e8aedac58678508161
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-18 02:33:10 -0300

    🚀 Project cards developed

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,8 +3,27 @@ import Message from "../layout/Message";
 import styles from './Projects.module.css'
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
+import { useState, useEffect } from 'react'
 
 export default function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            content: {
+                'Content-Type': 'application/json',
+            },
+        }).then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => { console.log(err) })
+    }, [])
+
     const location = useLocation()
     let message = ''
     if (location.state) {
@@ -18,7 +37,14 @@ export default function Projects() {
             </div>
             {message && <Message msg={message} type="success" />}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => <ProjectCard key={project.id}
+                        name={project.name}
+                        id={project.id}
+                        budget={project.budget}
+                        category={project.category.name}
+                    />)
+                }
             </Container>
         </div>
     )
